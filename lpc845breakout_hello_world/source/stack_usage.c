e582f037b10e222a884e239ca18b8336c9ba923c
andeyqi <qihengchuan8888@163.com>
2023-04-22 21:34:55 +0800

    update stack/perf command

--- lpc845breakout_hello_world/source/stack_usage.c
+++ lpc845breakout_hello_world/source/stack_usage.c
@@ -24,64 +24,60 @@ struct task_stack_info
     uint32_t*  stack_tail;
 };
 
-#define OS_NUM_TASKS 4
+struct task_stack_info_tbl
+{
+    uint8_t num;
+    struct task_stack_info * tasks;
+};
 
-static struct task_stack_info stack_all[OS_NUM_TASKS+2]; //add timer & idle
+#define OS_NUM_TASKS_SUPPORT 10
+
+static struct task_stack_info stack_all[OS_NUM_TASKS_SUPPORT];
+
+static struct task_stack_info_tbl task_stacks =
+{
+    .num = 0,
+    .tasks = stack_all,
+};
+
+
+#define TASK_STACK_NAME(i) task_stacks.tasks[i].task_name
+#define TASK_STACK_DEEP(i) task_stacks.tasks[i].stack_deep
+#define TASK_STACK_TAIL(i) task_stacks.tasks[i].stack_tail
+#define TASK_STACK_TAIL_OFF_ADDR(i,off) &task_stacks.tasks[i].stack_tail[off]
+
 
 
 void task_create_hook_stack(char * name ,unsigned int deep,unsigned int * stack_tail)
 {
-    static uint8_t i_loop = 0;
+    uint8_t i_loop = task_stacks.num;
     uint8_t len,i;
+    char * p_task_name;
 
-    if(i_loop < OS_NUM_TASKS+2)
+    if(i_loop < OS_NUM_TASKS_SUPPORT)
     {
-        strcpy(stack_all[i_loop].task_name,name);
-        if((len = strlen(stack_all[i_loop].task_name)) != (configMAX_TASK_NAME_LEN-1))
+        p_task_name = TASK_STACK_NAME(i_loop);
+        strcpy(TASK_STACK_NAME(i_loop),name);
+        if((len = strlen(TASK_STACK_NAME(i_loop))) != (configMAX_TASK_NAME_LEN-1))
         {
             for(i = len;i < configMAX_TASK_NAME_LEN-1;i++)
-                stack_all[i_loop].task_name[i] = '\0';
+                p_task_name[i] = '\0';
         }
-        stack_all[i_loop].task_name[configMAX_TASK_NAME_LEN-1] = '\0';
-        stack_all[i_loop].stack_deep = deep*4;
-        stack_all[i_loop].stack_tail = stack_tail;
+        p_task_name[configMAX_TASK_NAME_LEN-1] = '\0';
+        TASK_STACK_DEEP(i_loop) = deep*4;
+        TASK_STACK_TAIL(i_loop) = stack_tail;
         /* if use pref to set perf default value */
 #if ( configUSE_PERF_CNT == 1 )
-        memset((void*)stack_all[i_loop].stack_tail,0,sizeof(task_cycle_info_t)+8);
+        memset((void*)TASK_STACK_TAIL(i_loop),0,sizeof(task_cycle_info_t)+8);
 #endif
-        i_loop++;
+        task_stacks.num++;
     }
     else
     {
-        PRINTF("array is overflow \r\n");
+        PRINTF("task num is max than OS_NUM_TASKS_SUPPORT.\r\n");
     }
 }
 
-#if 0
-void get_cur_thread_stack_info(uint32_t sp, uint32_t *start_addr, size_t *size)
-{
-    uint8_t i_loop = 0;
-    extern __no_init volatile char switchintaskname[configMAX_TASK_NAME_LEN];
-
-    for(i_loop = 0;i_loop< OS_NUM_TASKS+2;i_loop++)
-    {
-        if(strcmp(stack_all[i_loop].task_name,(const char *)switchintaskname) == 0)
-            break;
-        if((sp > (uint32_t)stack_all[i_loop].stack_tail) && ( sp < ((uint32_t)stack_all[i_loop].stack_tail)+stack_all[i_loop].stack_deep))
-            break;
-    }
-    /* find stack info update */
-    if(i_loop != OS_NUM_TASKS+2)
-    {
-        *start_addr = (uint32_t)stack_all[i_loop].stack_tail;
-        *size = stack_all[i_loop].stack_deep;
-    }
-    else
-    {
-         *size = 0;
-    }
-}
-#endif
 
 static int buff_continuous_numbers(uint8_t * buff,uint8_t data)
 {
@@ -107,23 +103,25 @@ unsigned int stack(char argc,char ** argv)
     {
         //logctrl_instance()->is_sync = 1;
         PRINTF("taskname\tdeep\tused\taddress\t\t\tusage\r\n");
-        for(i = 0;i < OS_NUM_TASKS+2;i++)
+        for(i = 0;i < task_stacks.num;i++)
         {
 #if ( configUSE_PERF_CNT != 1 )
-            len = buff_continuous_numbers((uint8_t *)stack_all[i].stack_tail,0xa5);
+            len = buff_continuous_numbers((uint8_t *)TASK_STACK_TAIL(i),0xa5);
 #else
-            len = buff_continuous_numbers((uint8_t *)&stack_all[i].stack_tail[WORD_OF_CYCEL_INFO],0xa5);
+            len = buff_continuous_numbers((uint8_t *)TASK_STACK_TAIL_OFF_ADDR(i,WORD_OF_CYCEL_INFO),0xa5);
             if(len)
                 len += BYTE_OF_CYCEL_INFO;
 #endif
-        strcpy(task_name,stack_all[i].task_name);
+        strcpy(task_name,TASK_STACK_NAME(i));
         task_name[configMAX_TASK_NAME_LEN - 1] = '\0';
-        if((name_len = strlen(stack_all[i].task_name)) != (configMAX_TASK_NAME_LEN-1))
+        if((name_len = strlen(TASK_STACK_NAME(i))) != (configMAX_TASK_NAME_LEN-1))
         {
             for(j = name_len;j < configMAX_TASK_NAME_LEN-1;j++)
                 task_name[j] = ' ';
         }
-            PRINTF("%s\t%d\t%d\t0x%p~0x%p\t%d%%\r\n",task_name,stack_all[i].stack_deep,stack_all[i].stack_deep-len,stack_all[i].stack_tail,stack_all[i].stack_tail+(stack_all[i].stack_deep/4),(100-(len*100)/stack_all[i].stack_deep));
+            PRINTF("%s\t%d\t%d\t0x%p~0x%p\t%d%%\r\n",task_name,TASK_STACK_DEEP(i),
+            TASK_STACK_DEEP(i)-len,TASK_STACK_TAIL(i),
+            TASK_STACK_TAIL(i)+(TASK_STACK_DEEP(i)/4),(100-(len*100)/TASK_STACK_DEEP(i)));
         }
         PRINTF("\r\nHeap Total:%d\tFree:%d\r\n",configTOTAL_HEAP_SIZE,xPortGetFreeHeapSize());
         //logctrl_instance()->is_sync = 0;
@@ -131,11 +129,11 @@ unsigned int stack(char argc,char ** argv)
     if(argc == 2)
     {
         uint8_t index = atoi(argv[1]);
-        if(index >=  OS_NUM_TASKS+2)
+        if(index >=  task_stacks.num)
             goto out;
-        PRINTF("task name [%s] stack deep 0x%x\r\n",stack_all[index].task_name,stack_all[index].stack_deep);
+        PRINTF("task name [%s] stack deep 0x%x\r\n",TASK_STACK_NAME(index),TASK_STACK_DEEP(index));
         //logctrl_instance()->is_sync = 1;
-        trace_byte_stream((uint8_t *)stack_all[index].stack_tail,stack_all[index].stack_deep,0);
+        trace_byte_stream((uint8_t *)TASK_STACK_TAIL(index),TASK_STACK_DEEP(index),0);
         //logctrl_instance()->is_sync = 0;
     }
 out:
@@ -156,12 +154,12 @@ unsigned int perf(char argc,char ** argv)
     if(argc == 1)
     {
         PRINTF("taskname\trecent\t\tactive\tusedmax\t\tusedmin\t\tusedavg\t\tcpuusage\r\n");
-        for(i = 0;i < OS_NUM_TASKS+2;i++)
+        for(i = 0;i < task_stacks.num;i++)
         {
-            task_cycle_info_t * pcycle = get_rtos_task_cycle_info_by_stack(stack_all[i].stack_tail);
+            task_cycle_info_t * pcycle = get_rtos_task_cycle_info_by_stack(TASK_STACK_TAIL(i));
             task_name[configMAX_TASK_NAME_LEN - 1] = '\0';
-            strcpy(task_name,stack_all[i].task_name);
-            if((len = strlen(stack_all[i].task_name)) != (configMAX_TASK_NAME_LEN-1))
+            strcpy(task_name,TASK_STACK_NAME(i));
+            if((len = strlen(TASK_STACK_NAME(i))) != (configMAX_TASK_NAME_LEN-1))
             {
                 for(j = len;j < configMAX_TASK_NAME_LEN-1;j++)
                     task_name[j] = ' ';
