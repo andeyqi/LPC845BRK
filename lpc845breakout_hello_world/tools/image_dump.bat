c53806121c977c792db5b915da90a4d39defa3ed
andeyqi <qihengchuan8888@163.com>
2023-05-08 09:20:27 +0800

    update image_bat gnu path

--- lpc845breakout_hello_world/tools/image_dump.bat
+++ lpc845breakout_hello_world/tools/image_dump.bat
@@ -3,7 +3,8 @@
 SET BUILD_OUTPUT_DIR=..\Debug
 SET BUIND_EXE_NAME=lpc845breakout_hello_world
 
-SET GNU_BUIDLD_INS_DIR=D:\work\gcc-arm-none-eabi-10.3-2021.10\bin
-SET PATH=%PATH%;%READ_ELF_INS_DIR%
+::If the gcc path environment variable is not set, add it as follows
+::SET GNU_BUIDLD_INS_DIR=D:\work\gcc-arm-none-eabi-10.3-2021.10\bin
+::SET PATH=%PATH%;%READ_ELF_INS_DIR%
 
 arm-none-eabi-objdump.exe  %BUILD_OUTPUT_DIR%\%BUIND_EXE_NAME%.axf   -S  -a  > %BUILD_OUTPUT_DIR%\%BUIND_EXE_NAME%.asm
