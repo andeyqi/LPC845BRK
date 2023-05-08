05eaa066b0fb1fdb86e2c90d0781801a44b50c60
andeyqi <qihengchuan8888@163.com>
2023-05-09 07:39:50 +0800

    add common

--- lpc845breakout_hello_world/tools/image_dump.py
+++ lpc845breakout_hello_world/tools/image_dump.py
@@ -1,4 +1,5 @@
 import os
 
 if __name__ == '__main__':
+    #If the gcc path environment variable is not set, add it as follows
     os.system("arm-none-eabi-objdump.exe  ..\Debug\lpc845breakout_hello_world.axf   -S  -a  > ..\Debug\lpc845breakout_hello_world_py.asm")
